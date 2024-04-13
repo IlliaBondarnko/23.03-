6735fff6fe62a312a4a9bb08d53242d4f0a433fc
IlliaBondarnko <skyarcher007@gmail.com>
2024-04-13 20:00:51 +0300

    football_failed

--- pj.js
+++ pj.js
@@ -38,3 +38,44 @@ function getIntoHours(min) {
 // console.log(getIntoHours(70))
 
 // ________________________________________________________________
+// ___FOOTBALL_____________________________________________________
+
+const football_field = document.getElementById("football_field");
+
+const football = document.getElementById("football");
+
+football_field.addEventListener("click", whenClicked);
+
+
+function whenClicked(event) {
+
+  const field_client_x = event.clientX;
+  const field_client_y = event.clientY;
+
+  const ball_client_width = football.clientWidth;
+  const ball_client_height = football.clientHeight;
+
+  const field_offset_left = football_field.offsetLeft;
+  const field_offset_top = football_field.offsetTop;
+
+  const mouse_width = event.clientX - field_offset_left
+  const mouse_height = event.clientY - field_offset_top
+
+  console.log(mouse_width, mouse_height)
+
+  // football.style.transform = `translate(${mouse_width - ball_client_width}px, ${mouse_height - ball_client_height}px)`
+
+  const newX = Math.max(ball_client_width / 2, Math.min(mouse_width, field_client_x - ball_client_width / 2));
+      const newY = Math.max(ball_client_height / 2, Math.min(mouse_height, field_client_y - ball_client_height / 2));
+
+      football.style.transform = `translate(${newX - ball_client_width / 2}px, ${newY - ball_client_height / 2}px)`;
+}
+
+
+
+
+
+
+
+
+// ________________________________________________________________
