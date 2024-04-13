a9b8ecfadae940345ef9128fe02d8972d4745dcb
IlliaBondarnko <skyarcher007@gmail.com>
2024-04-13 18:21:06 +0300

    time_calc added

--- pj.js
+++ pj.js
@@ -11,3 +11,30 @@ window.onclick = function(e) {
     }
   }
 }
+
+// ________calc_time__________________________________________________
+
+const time_calc_input = document.getElementById("time_calc_input_id")
+
+const time_calc_button = document.getElementById("time_calc_button_id")
+
+time_calc_button.addEventListener("click", timeCalcOnClic)
+
+const answear_input = document.getElementById("final_time")
+
+function timeCalcOnClic() {
+  const input_value = Number(time_calc_input.value)
+  const final_functional_result = getIntoHours(input_value)
+  answear_input.textContent = final_functional_result
+}
+
+
+function getIntoHours(min) {
+  const result = Math.floor(min / 60)
+  const final_result = min - (60*result)
+  return `${result}h and ${final_result}min`
+}
+
+// console.log(getIntoHours(70))
+
+// ________________________________________________________________
